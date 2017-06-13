9fb0981d0567c6b663470fb54c6cd5ea54a7e06d
Guille Valencia <guivalen@gmail.com>
2017-06-13 07:34:15 -0500

    404

---
+++ cloud/404.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body style="background-image: url('views/assets/images/404_05.jpg'); background-size: cover; "> 
+  </body>
+</html>
