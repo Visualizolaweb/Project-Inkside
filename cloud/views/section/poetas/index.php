f0cdd0b72ef14e59ef5b328ec0aa3c32d8679589
Claudia Giraldo <claom397@hotmail.com>
2017-07-16 14:02:06 -0500

    style comunity

--- cloud/views/section/poetas/index.php
+++ cloud/views/section/poetas/index.php
@@ -42,106 +42,89 @@
 
 <script> document.title = 'Conoce los poemas de <?php echo $perfil['poet_nick']; ?>'; </script>
 
-<div class="container detail-poet">
-  <section class="profile row">
-    <div class="col s12 m3 l3 center-align"> <img src="<?php echo $avatar ?>" class="circle" alt="<?php echo $perfil['poet_nick']; ?>"> </div>
-    <div class="col s12 m9 l9">
-      <div class="row">
-        <div class="col s12"><h1><?php echo $perfil['poet_nick']?></h1></div>
-        <div class="col m6 hide-on-small-only"><button onclick="followPoet()" type="button" class="center waves-effect waves-light btn amber accent-3 z-depth-0 btn-icon"><i class="fa fa-plus icon-button orange"></i>Seguir al poeta</button></div>
-        <div class="col m6 hide-on-small-only">
-          <ul>
-            <li><?php echo $totalPoemas?> Poemas</li>
-            <li><?php echo $numeroSeguidores?> Seguidores</li>
-          </ul>
+<section id="wrap-container">
+  <div class="row container-fluid">
+    <div class="col m12 header-section detail-poet">
+        <h5 class="title">Conoce los poemas de <?php echo $perfil['poet_nick']?></h5>
+        <div class="row">
+          <div class="col m2 profile-poet"><img src="<?php echo $avatar ?>" class="circle" alt="<?php echo $perfil['poet_nick']; ?>"></div>
+          <div class="col m10"><?php echo $perfil['pdesc_acerca']?></div>
         </div>
-        <div class="col s12">
-          <p><?php echo $perfil['pdesc_acerca']?></p>
-        </div>
-      </div>
     </div>
-  </section>
-</div>
 
-<section class="sec-publicaciones secundary-default">
-  <div class="container">
+    <div class="sec-publicaciones  comunity ">
+
     <div class="row">
-      <section class="pinBoot">
-        <?php
-               foreach ($poemas as $row) {
-                 if ($row['pdesc_avatar']=='') {
-                   $delimitador = explode("/",$row['poet_foto']);
-                   if(($delimitador[0] == 'https:') OR ($delimitador[0] == 'http:')){
-                     $avatar = $row['poet_foto'];
+        <section class="pinBoot">
+          <?php
+                 foreach ($poemas as $row) {
+                   if ($row['pdesc_avatar']=='') {
+                     $delimitador = explode("/",$row['poet_foto']);
+                     if(($delimitador[0] == 'https:') OR ($delimitador[0] == 'http:')){
+                       $avatar = $row['poet_foto'];
+                     }else{
+                       $avatar = "cloud/".$row['poet_foto'];
+                     }
                    }else{
-                     $avatar = "cloud/".$row['poet_foto'];
+                     $avatar = 'cloud/'.$row['pdesc_avatar'];
                    }
-                 }else{
-                   $avatar = 'cloud/'.$row['pdesc_avatar'];
-                 }
 
-                 if(isset($row['pub_imgPortada'])){
-                   $image = "cloud/views/assets/images/portadasPoemas/".$row['pub_imgPortada'];
-                   if(file_exists($image)){
-                     $portada = '<img src="'.$image.'" alt="'.$row['pub_titulo'].'">';
+                   if(isset($row['pub_imgPortada'])){
+                     $image = "cloud/views/assets/images/portadasPoemas/".$row['pub_imgPortada'];
+                     if(file_exists($image)){
+                       $portada = '<img src="'.$image.'" alt="'.$row['pub_titulo'].'">';
+                     }else{
+                       $portada = "";
+                     }
                    }else{
                      $portada = "";
                    }
-                 }else{
-                   $portada = "";
-                 }
 
-                 $contenido = $publicaciones->getSubString($row['pub_contenido']);    ?>
+                   $contenido = $publicaciones->getSubString($row['pub_contenido']);    ?>
 
-        <article class="white-panel">
-          <?php echo $portada; ?>
-          <h2><a href="pubID<?php echo $row['pub_codigo']; ?>"><?php echo $row['pub_titulo'];?></a></h2>
-          <span class="date">Publicado el <?php echo $publicaciones->fechaesp($row['pub_fechaPublicacion']); ?></span>
-          <p><?php echo $contenido ?>
-              <div class="more"><a href="pubID<?php echo $row['pub_codigo']; ?>" class="read-more">Seguir Leyendo</a></div>
-          </p>
-        </article>
-        <?php } ?>
+                <article class="white-panel">
+                  <?php echo $portada; ?>
+                  <h2><a href="pubID<?php echo $row['pub_codigo']; ?>"><?php echo $row['pub_titulo'];?></a></h2>
+                  <span class="date">Publicado el <?php echo $publicaciones->fechaesp($row['pub_fechaPublicacion']); ?></span>
+                  <p><?php echo $contenido ?>
+                      <div class="more"><a href="pubID<?php echo $row['pub_codigo']; ?>" class="read-more">Seguir Leyendo</a></div>
+                  </p>
+                </article>
+          <?php } ?>
 
-      </section>
+        </section>
       <?php
-      if(isset($paginador)){
-        if($paginador == true){
+          if(isset($paginador)){
+            if($paginador == true){
 
-        echo '<p><hr></p>
-       <div style="width:100%; text-align:center;">';
-       //si posicion es mayor o igual a 1 quiere decir que muestre la parte Primero y Anterior de la paginación
-       if ($pagina >= 1) {
-         $url = "index.php?c=views&a=cargarPublicacion&pagina=0";
-         echo "<a href=\"$url\">Primero</a>\n";
-         //para que el preius no termine con valor 0
-          $url = "index.php?c=views&a=cargarPublicacion&pagina=" .($pagina-1);
-         echo "<a href=\"$url\">Anterior</a>\n";
-       }
+            echo '<p><hr></p>
+           <div style="width:100%; text-align:center;">';
+           //si posicion es mayor o igual a 1 quiere decir que muestre la parte Primero y Anterior de la paginación
+           if ($pagina >= 1) {
+             $url = "index.php?c=views&a=cargarPublicacion&pagina=0";
+             echo "<a href=\"$url\">Primero</a>\n";
+             //para que el preius no termine con valor 0
+              $url = "index.php?c=views&a=cargarPublicacion&pagina=" .($pagina-1);
+             echo "<a href=\"$url\">Anterior</a>\n";
+           }
 
-       echo '<strong> Página '.($pagina).' de '.$total_paginas.' </strong>';
+           echo '<strong> Página '.($pagina).' de '.$total_paginas.' </strong>';
 
-       //si position es menor a el valor entre los parentesis muestra la parte (Siguiente Ultimo)
-       if ($pagina < ($total_paginas-1)) {
+           //si position es menor a el valor entre los parentesis muestra la parte (Siguiente Ultimo)
+           if ($pagina < ($total_paginas-1)) {
 
-         $url = "index.php?c=views&a=perfilPoeta&codpoet=".$_GET['codpoet']."&pagina=" . ($pagina+1);
-         echo "<a href=\"$url\">Siguiente</a>\n";
-         $url = "index.php?c=views&a=perfilPoeta&codpoet=".$_GET['codpoet']."&pagina=" . ($total_paginas-1);
-         echo "<a href=\"$url\">Ultimo</a>\n";
-       }
-       echo '</div>';
-      }
-    }
+             $url = "index.php?c=views&a=perfilPoeta&codpoet=".$_GET['codpoet']."&pagina=" . ($pagina+1);
+             echo "<a href=\"$url\">Siguiente</a>\n";
+             $url = "index.php?c=views&a=perfilPoeta&codpoet=".$_GET['codpoet']."&pagina=" . ($total_paginas-1);
+             echo "<a href=\"$url\">Ultimo</a>\n";
+           }
+           echo '</div>';
+          }
+        }
       ?>
     </div>
+
+</div>
+
   </div>
 </section>
-
-<!-- ESTRUCTURA MODAL SEGUIR POETA -->
-
-<div id="modalFollow" class="modal" style="max-height: 100%;">
-  <div class="modal-content">
-    <h4>Registrate e inicia sesión en Inkside!</h4>
-    <p>Lo sentimos, para seguir un poeta te invitamos a que te registres de forma gratuita en nuestra comunidad.</p>
-  </div>
-</div>
